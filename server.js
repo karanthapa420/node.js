c5bcef5e1f0afa4a93446764bd226f0be2cf5e82
karanthapa420 <karanthapa4209211@gmail.com>
2024-08-01 10:55:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const SECRET_KEY = '8eb3010d717a9689df16a166f91f52728b4db10474a5ecde';
 const affiliateLinks = [
   'https://stake.com/?c=Yh63e0CV',
@@ -15,6 +15,7 @@ const affiliateLinks = [
   'https://t.ajrkm.link/328984/7683/0?bo=2779,2778,2777,2776,2775&po=6533&aff_sub5=SF_006OG000004lmDN'
 ];
 
+// Middleware to parse token
 function parseToken(req, res, next) {
   const token = req.query.key || req.query.token;
   if (!token) {
@@ -30,21 +31,30 @@ function parseToken(req, res, next) {
   }
 }
 
+// Redirect the main domain to Google
+app.get('/', (req, res) => {
+  res.redirect('https://www.google.com');
+});
+
+// Endpoint to handle the redirection functionality
 app.get('/api/users', parseToken, (req, res) => {
   const randomLink = affiliateLinks[Math.floor(Math.random() * affiliateLinks.length)];
   res.redirect(randomLink);
 });
 
+// Function to create token
 function createToken(data) {
   return jwt.sign(data, SECRET_KEY, { expiresIn: '24h' });
 }
 
+// Endpoint to generate token and redirect
 app.get('/kkt4pmb', (req, res) => {
   const token = createToken({ userId: 123 });
   const redirectUrl = `/n4vk1wub0?key=${token}`;
   res.redirect(redirectUrl);
 });
 
+// Endpoint to verify token and send file
 app.get('/n4vk1wub0', (req, res) => {
   const token = req.query.key;
   if (!token) {
@@ -60,6 +70,7 @@ app.get('/n4vk1wub0', (req, res) => {
   }
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
